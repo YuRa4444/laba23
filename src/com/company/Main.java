d00dcef438e7473dfdb0be3fdfe442106cccb9fa
Yura <102286821+YuRa4444@users.noreply.github.com>
2022-03-24 12:07:18 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Завдання 1
+        int a = 105;
+        int b = 5;
+        int c = a / b;
+        int d = a % b;
+
+        System.out.println("Завдання 1: "+c+"; "+d);
+
+        //Завдання 2
+        int f = 42;
+        int h = 42 % 10;
+        int x = (f/10)%10;
+
+        System.out.println("Завдання 2: "+(f+h));
+
+        //Завдання 3
+        double k = 9.3;
+        double m = Math.round(k);
+
+        System.out.println("Завдання 3: "+m);
+
+    }
+
+
+
+}
